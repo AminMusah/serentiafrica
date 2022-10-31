d3a458425eb1998fa8e364cf84945a9df4beb7fb
AminMusah <ameenmusah77@gmail.com>
2022-10-31 11:20:27 -0500

    update about us page

--- main.js
+++ main.js
@@ -80,3 +80,7 @@ const revealOnScroll = function () {
 window.addEventListener("scroll", revealOnScroll);
 
 revealOnScroll();
+
+const year = document.getElementById("year");
+const currentYear = new Date();
+year.innerHTML = currentYear.getFullYear()
